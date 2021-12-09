93d31906d2e93471d361986f31e6b8ec87fe276a
Leng Thodsapon <82572605+lengtsp@users.noreply.github.com>
2021-12-09 20:57:15 +0700

    Update hello_lib.py

--- hello_lib.py
+++ hello_lib.py
@@ -8,17 +8,18 @@ def now():
     time_str = now1.strftime('%H:%M:%S')
     return "%d %s %d %s"%(now1.day, month_name, thai_year, time_str) # 30 ตุลาคม 2560 20:45:30
     
-def all_lib():
-#     ls drive
-    pip install pydrive
-    pip install xlsxwriter
-    import pandas as pd
-    import xlsxwriter
-    from pydrive.auth import GoogleAuth
-    from pydrive.drive import GoogleDrive 
-    from google.colab import auth 
-    from oauth2client.client import GoogleCredentials
-    auth.authenticate_user()
-    gauth = GoogleAuth()
-    gauth.credentials = GoogleCredentials.get_application_default()
-    drive = GoogleDrive(gauth)
+
+     
+!ls drive
+!pip install pydrive
+!pip install xlsxwriter
+import pandas as pd
+import xlsxwriter
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive 
+from google.colab import auth 
+from oauth2client.client import GoogleCredentials
+auth.authenticate_user()
+gauth = GoogleAuth()
+gauth.credentials = GoogleCredentials.get_application_default()
+drive = GoogleDrive(gauth)
