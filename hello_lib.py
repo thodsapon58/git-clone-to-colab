630da67a0dcca26d42e0f479a89b0c3af569c044
Leng Thodsapon <82572605+lengtsp@users.noreply.github.com>
2021-12-09 23:03:43 +0700

    Update hello_lib.py

--- hello_lib.py
+++ hello_lib.py
@@ -95,6 +95,6 @@ def add_log(
     
 def df_cutheader_secondrow(df, col_index, col_name, cut_name ):
     df         = df.rename(columns={df.columns[col_index] : col_name}      
-    cut_querymain_header = [cut_name]
+    cut_querymain_header = cut_name
     df = df[   (~df[col_name].isin(cut_querymain_header)) ]  # ~ แปลวา not คือไม่เอารายการใน list นั่นมาแสดง
     return df
