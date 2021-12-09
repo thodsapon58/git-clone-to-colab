7226bc563a88aaf03f33626b1cd543a4a0f961b6
Leng Thodsapon <82572605+lengtsp@users.noreply.github.com>
2021-12-09 20:48:16 +0700

    Update and rename import_library.ipynb to hello_lib.py

--- hello_lib.py
+++ hello_lib.py
@@ -8,18 +8,17 @@ def now():
     time_str = now1.strftime('%H:%M:%S')
     return "%d %s %d %s"%(now1.day, month_name, thai_year, time_str) # 30 ตุลาคม 2560 20:45:30
     
-    
-!ls drive
-!pip install pydrive
-!pip install xlsxwriter
-import pandas as pd
-import xlsxwriter
-# these classes allow you to request the Google drive API
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive 
-from google.colab import auth 
-from oauth2client.client import GoogleCredentials
-auth.authenticate_user()
-gauth = GoogleAuth()
-gauth.credentials = GoogleCredentials.get_application_default()
-drive = GoogleDrive(gauth)
+def all_lib():
+    !ls drive
+    !pip install pydrive
+    !pip install xlsxwriter
+    import pandas as pd
+    import xlsxwriter
+    from pydrive.auth import GoogleAuth
+    from pydrive.drive import GoogleDrive 
+    from google.colab import auth 
+    from oauth2client.client import GoogleCredentials
+    auth.authenticate_user()
+    gauth = GoogleAuth()
+    gauth.credentials = GoogleCredentials.get_application_default()
+    drive = GoogleDrive(gauth)
