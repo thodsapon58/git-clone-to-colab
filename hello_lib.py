f2a2ad2ddd59407decd92f0072d94d7b193a13a2
Leng Thodsapon <91821752+thodsapon58@users.noreply.github.com>
2022-02-02 15:47:25 +0700

    Update hello_lib.py

--- hello_lib.py
+++ hello_lib.py
@@ -325,7 +325,6 @@ def upload_file_split_sheet(df, id_googlesheet, f1_value, f2, f3, f3_value, my_s
                                         '3.3 คงเหลือ - สิทธิการลาพักผ่อนสะสมใช้ได้ 3 ปี' : field33,     
                                     
     })
-    df_new[field34] = df_new[field34].astype(float)
     df_new[field34] = 0.0
     df_new[field34] = (df_new[field32] / df_new[field31])
     
@@ -349,7 +348,7 @@ def upload_file_split_sheet(df, id_googlesheet, f1_value, f2, f3, f3_value, my_s
                                 field31,
                                 field32,
                                 field33,
-                                field34
+                                field34,
 #                                 'หน่วยงานที่ดูแล'
                               ]
     df_new = df_new.drop(['หน่วยงานที่ดูแล'],axis =1)
