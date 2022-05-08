991e0001c15d1c9cdb844d6edf5b9547f2cb4904
Leng Thodsapon <91821752+thodsapon58@users.noreply.github.com>
2022-05-08 18:58:58 +0700

    Create webclass.py

---
+++ webscrapping/webclass.py
@@ -0,0 +1,22 @@
+class clssAccessWebsite():
+    base_url = "https://intranet.krungthai/intranet/index2.html"
+    
+    def setUp(self):
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+        self.driver.implicitly_wait(10)
+        
+    def load_home_page(self):
+        driver = self.driver
+        driver.get(self.base_url)
+        
+        
+    def reset_page(self):
+        time.sleep(4)
+#         id_submit=self.driver.find_element(By.XPATH,  '//*[@id="searchbox"]')
+
+        self.driver.find_element_by_xpath('//*[@id="searchBox"]').click()
+    
+        self.driver.find_element_by_xpath('//*[@id="searchBox"]').send_keys("xxx")
+        self.driver.find_element_by_xpath('//*[@id="searchBox"]').send_keys(Keys.RETURN)
+        time.sleep(1)
+        
